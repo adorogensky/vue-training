4ac4b8a4dfbddfef9a2661885fe97bc530165020
Alex Dorogensky <alex@exebar.com>
2020-09-15 22:36:43 -0500

    import data from json, display threads and posts with user under each thread

---
+++ src/components/Forum.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Welcome to the Forum</h1>
+    <div :key="thread.id" v-for="thread in threads">
+      <h2>{{ thread.title }}</h2>
+      <div :key="postId" v-for="postId in thread.posts">
+        <p>{{users[posts[postId].userId].name}}</p>
+        <p>{{posts[postId].text}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import data from '../data.json'
+
+export default {
+  name: 'Forum',
+  data () {
+    return {
+      threads: data.threads,
+      posts: data.posts,
+      users: data.users
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Forum from '@/components/Forum'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Forum',
+      component: Forum
     }
   ]
 })
