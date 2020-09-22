199d0f1945554978f825e3bcff61549ae03f79bd
Alex Dorogensky <alex@exebar.com>
2020-09-22 17:57:43 -0500

    rename Thread.vue => Threads.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Thread from '@/components/Thread'
+import Threads from '@/components/Threads'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Thread',
-      component: Thread
+      name: 'Threads',
+      component: Threads
     }
   ]
 })
