e6eb7a5beb6ffb503aa3858c204379a1f1c13b70
Alex Dorogensky <alex@exebar.com>
2020-09-22 21:05:49 -0500

    Thread.vue: new component to display thread posts (depends on this.$route.params.id) router/index.js: /thread/:id now routes to Thread.vue

---
+++ src/components/Thread.vue
@@ -0,0 +1,43 @@
+<template>
+    <NotFound v-if="thread === undefined" />
+    <div v-else class="col-large push-top">
+      <h1> {{ thread.title }} </h1>
+      <div class="post-list">
+        <div v-for="postId in thread.posts" :key="postId" class="post">
+          <div class="user-info">
+            <a href="#" class="user-name">{{users[posts[postId].userId].name}}</a>
+            <a href="#">
+              <img class="avatar-large" :src="users[posts[postId].userId].avatar" alt="">
+            </a>
+            <p class="desktop-only text-small">107 posts</p>
+          </div>
+          <div class="post-content">
+            <div>
+              <p>{{ posts[postId].text }}</p>
+            </div>
+          </div>
+
+          <div class="post-date text-faded">
+            6 hours ago
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+<script>
+import NotFound from '@/components/NotFound'
+import data from '@/data'
+export default {
+    name: 'Thread',
+    components: {
+        NotFound
+    },
+    data() {
+        return {
+            thread: data.threads[this.$route.params.id],
+            users: data.users,
+            posts: data.posts
+        }
+    }
+}
+</script>
