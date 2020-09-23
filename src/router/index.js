24bc4a19b740f843aa58f0a50fc544b90e612398
Alex Dorogensky <alex@exebar.com>
2020-09-22 20:11:05 -0500

    add new component NotFound that displays a static message router/index.js: fall back to NotFound component for non-matching routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Threads from '@/components/Threads'
+import NotFound from '@/components/NotFound'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Threads',
-      component: Threads
-    }
-  ],
+  routes: [{
+    path: '/',
+    name: 'Threads',
+    component: Threads
+  }, {
+    path: '*',
+    name: 'NotFound',
+    component: NotFound
+  }],
   mode: 'history'
 })
