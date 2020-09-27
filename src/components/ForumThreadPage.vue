08de44ab0664bd0465d9f346c2ecb251f6afb677
Alex Dorogensky <alex@exebar.com>
2020-09-27 18:04:41 -0500

    ForumThreaPage.vue: now uses a new component ForumThreadPost to display post contents. Some posts' styles look different now

--- src/components/ForumThreadPage.vue
+++ src/components/ForumThreadPage.vue
@@ -4,34 +4,20 @@
       <h1> {{ thread.title }} </h1>
       <div class="post-list">
         <div v-for="postId in thread.posts" :key="postId" class="post">
-          <div class="user-info">
-            <a href="#" class="user-name">{{users[posts[postId].userId].name}}</a>
-            <a href="#">
-              <img class="avatar-large" :src="users[posts[postId].userId].avatar" alt="">
-            </a>
-            <p class="desktop-only text-small">107 posts</p>
-          </div>
-          <div class="post-content">
-            <div>
-              <p>{{ posts[postId].text }}</p>
-            </div>
-          </div>
-
-          <div class="post-date text-faded">
-            6 hours ago
-          </div>
+          <forum-thread-post :user="users[posts[postId].userId]" :post="posts[postId]" />
         </div>
       </div>
     </div>
 </template>
 <script>
 import NotFoundPage from '@/components/NotFoundPage'
+import ForumThreadPost from '@/components/ForumThreadPost'
 import data from '@/data'
 export default {
     name: 'ForumThreadPage',
     props: ['id'],
     components: {
-        NotFoundPage
+        NotFoundPage, ForumThreadPost
     },
     data() {
         return {
