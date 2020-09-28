877309ca1701c2ebdef8cb782cc9706f6a4c7520
Alex Dorogensky <alex@exebar.com>
2020-09-28 14:59:12 -0500

    ForumThreadPostList.vue: rename => ThreadPosts.vue

--- src/components/ForumThreadPage.vue
+++ src/components/ForumThreadPage.vue
@@ -2,18 +2,18 @@
     <not-found-page v-if="thread === undefined" />
     <div v-else class="col-large push-top">
       <h1> {{ thread.title }} </h1>
-      <forum-thread-post-list :users="users" :posts="posts" />
+      <thread-posts :users="users" :posts="posts" />
     </div>
 </template>
 <script>
 import NotFoundPage from '@/components/NotFoundPage'
-import ForumThreadPostList from '@/components/ForumThreadPostList'
+import ThreadPosts from '@/components/ThreadPosts'
 import data from '@/data'
 export default {
     name: 'ForumThreadPage',
     props: ['id'],
     components: {
-        NotFoundPage, ForumThreadPostList
+        NotFoundPage, ThreadPosts
     },
     data() {
         return {
