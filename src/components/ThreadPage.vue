d78dc720148454370d9c9d33f01d18c8784a7a1c
Alex Dorogensky <alex@exebar.com>
2020-09-28 17:35:01 -0500

    ThreadPage.vue: add new post feature

--- src/components/ThreadPage.vue
+++ src/components/ThreadPage.vue
@@ -3,6 +3,14 @@
     <div v-else class="col-large push-top">
       <h1> {{ thread.title }} </h1>
       <thread-posts :users="users" :posts="posts" />
+      <form @submit.prevent="addPost">
+          <div class="form-group">
+              <textarea name="" id="" rows="10" class="form-input" v-model="newPostText"></textarea>
+          </div>
+          <div class="form-actions">
+              <button class="btn-blue">Submit Post</button>
+          </div>
+      </form>
     </div>
 </template>
 <script>
@@ -19,7 +27,27 @@ export default {
         return {
             thread: data.threads[this.id],
             users: data.users,
-            posts: data.posts
+            posts: data.posts,
+            newPostText: '',
+            postId: 0
+        }
+    },
+    methods: {
+        addPost() {
+            const newPostId = 'newPost' + this.postId
+            const post = {
+                text: this.newPostText,
+                userId: 'Miej9zSGMRZKDvMXzfxjVOyv3RF3',
+                threadId: this.threadId,
+                createdAt: Math.floor(Date.now() / 1000),
+                '.key': newPostId
+            }
+
+            this.$set(this.posts, newPostId, post);
+            this.$set(this.thread.posts, newPostId, post);
+            this.postId++
+
+            this.newPostText = '';
         }
     }
 }
