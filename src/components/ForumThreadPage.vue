2c7aa678c4fead3893a4f5e4230a340e518b1045
Alex Dorogensky <alex@exebar.com>
2020-09-28 14:33:30 -0500

    use ForumThreadPostList.vue, delete ForumThreadPost.vue

--- src/components/ForumThreadPage.vue
+++ src/components/ForumThreadPage.vue
@@ -2,22 +2,18 @@
     <not-found-page v-if="thread === undefined" />
     <div v-else class="col-large push-top">
       <h1> {{ thread.title }} </h1>
-      <div class="post-list">
-        <div v-for="postId in thread.posts" :key="postId" class="post">
-          <forum-thread-post :user="users[posts[postId].userId]" :post="posts[postId]" />
-        </div>
-      </div>
+      <forum-thread-post-list :users="users" :posts="posts" />
     </div>
 </template>
 <script>
 import NotFoundPage from '@/components/NotFoundPage'
-import ForumThreadPost from '@/components/ForumThreadPost'
+import ForumThreadPostList from '@/components/ForumThreadPostList'
 import data from '@/data'
 export default {
     name: 'ForumThreadPage',
     props: ['id'],
     components: {
-        NotFoundPage, ForumThreadPost
+        NotFoundPage, ForumThreadPostList
     },
     data() {
         return {
---
+++ src/components/ForumThreadPostList.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="post-list">
+        <div v-for="post in posts" :key="post['.key']" class="post">
+            <div class="user-info">
+                <a href="#" class="user-name">{{users[post.userId].name}}</a>
+                <a href="#">
+                    <img class="avatar-large" :src="users[post.userId].avatar" alt="">
+                </a>
+                <p class="desktop-only text-small">107 posts</p>
+            </div>
+            <div class="post-content">
+                <div>
+                    <p>{{ post.text }}</p>
+                </div>
+            </div>
+            <div class="post-date text-faded">
+                6 hours ago
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'ForumThreadPostList',
+    props: ['users', 'posts']
+}
+</script>
