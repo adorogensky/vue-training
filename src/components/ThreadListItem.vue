fbbd4508d44ad732eff37002e75c7c44e015c471
Alex Dorogensky <alex@exebar.com>
2020-09-29 11:25:10 -0500

    ThreadsPage.vue: use mockup html to display threads

---
+++ src/components/ThreadListItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="thread">
+    <div>
+      <p>
+        <a href="#">{{ thread.title }}</a>
+      </p>
+      <p class="text-faded text-xsmall">
+        By <a href="#">{{ user.name }}</a>, yesterday.
+      </p>
+    </div>
+
+    <div class="activity">
+      <p class="replies-count">1 reply</p>
+
+      <img class="avatar-medium" :src="user.avatar" alt="" />
+
+      <div>
+        <p class="text-xsmall">
+          <a href="profile.html">Bruce Wayne</a>
+        </p>
+        <p class="text-xsmall text-faded">2 hours ago</p>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+    props: ['thread', 'user']
+}
+</script>
--- src/components/ThreadsPage.vue
+++ src/components/ThreadsPage.vue
@@ -1,28 +1,164 @@
 <template>
-  <div>
-    <div v-for="thread in threads" :key="thread['.key']" class="col-large push-top">
-      <div class="post-list">
-        <thread-page :id="thread['.key']" />
+  <div class="col-full">
+    <div class="thread-list">
+      <h2 class="list-title">Threads</h2>
+
+      <div class="thread">
+        <div>
+          <p>
+            <a href="thread.html">How can I chop onions without crying?</a>
+          </p>
+          <p class="text-faded text-xsmall">
+            By <a href="profile.html">Joseph Kerr</a>, yesterday.
+          </p>
+        </div>
+
+        <div class="activity">
+          <p class="replies-count">
+            1 reply
+          </p>
+
+          <img
+            class="avatar-medium"
+            src="http://i0.kym-cdn.com/photos/images/facebook/000/010/934/46623-batman_pikachu_super.png"
+            alt=""
+          />
+
+          <div>
+            <p class="text-xsmall">
+              <a href="profile.html">Bruce Wayne</a>
+            </p>
+            <p class="text-xsmall text-faded">2 hours ago</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="thread">
+        <div>
+          <p>
+            <a href="thread.html">Wasabi vs horseraddish?</a>
+          </p>
+          <p class="text-faded text-xsmall">
+            By <a href="profile.html">Robin</a>, 8 hours ago
+          </p>
+        </div>
+
+        <div class="activity">
+          <p class="replies-count">
+            3 replies
+          </p>
+
+          <img
+            class="avatar-medium"
+            src="https://firebasestorage.googleapis.com/v0/b/forum-2a982.appspot.com/o/images%2Favatars%2Fraynathan?alt=media&amp;token=bd9a0f0e-60f2-4e60-b092-77d1ded50a7e"
+            alt=""
+          />
+          <span>
+            <a class="text-xsmall" href="profile.html">Ray-Nathan James</a>
+            <p class="text-faded text-xsmall">3 hours ago</p>
+          </span>
+        </div>
+      </div>
+
+      <div class="thread">
+        <div>
+          <p>
+            <a href="thread.html">Multifilling</a>
+          </p>
+          <p class="text-faded text-xsmall">
+            By <a href="profile.html">Ray-Nathan James</a>, 6 days ago
+          </p>
+        </div>
+
+        <div class="activity">
+          <p class="replies-count">
+            1 reply
+          </p>
+
+          <img
+            class="avatar-medium"
+            src="http://i0.kym-cdn.com/photos/images/facebook/000/010/934/46623-batman_pikachu_super.png"
+            alt=""
+          />
+          <span>
+            <a class="text-xsmall" href="profile.html">Bruce Wayne</a>
+            <p class="text-faded text-xsmall">6 days ago</p>
+          </span>
+        </div>
+      </div>
+
+      <div class="thread">
+        <div>
+          <p>
+            <a href="thread.html">Egg replacer for bread dough?</a>
+          </p>
+          <p class="text-faded text-xsmall">
+            By <a href="profile.html">Theodor Jackson</a>, 2 weeks ago
+          </p>
+        </div>
+
+        <div class="activity">
+          <p class="replies-count">
+            1 reply
+          </p>
+
+          <img
+            class="avatar-medium"
+            src="http://icons.iconarchive.com/icons/designbolts/free-male-avatars/128/Male-Avatar-icon.png"
+            alt=""
+          />
+          <span>
+            <a class="text-xsmall" href="profile.html">Theodor Jackson</a>
+            <p class="text-faded text-xsmall">2 weeks ago</p>
+          </span>
+        </div>
+      </div>
+
+      <div class="thread">
+        <div>
+          <p>
+            <a href="thread.html">Which is your favorite carbohydrate? 🤓</a>
+          </p>
+          <p class="text-faded text-xsmall">
+            By <a href="profile.html">Ray-Nathan James</a>, 1 month ago
+          </p>
+        </div>
+
+        <div class="activity">
+          <p class="replies-count">
+            0 replies
+          </p>
+
+          <img
+            class="avatar-medium"
+            src="http://i0.kym-cdn.com/photos/images/facebook/000/010/934/46623-batman_pikachu_super.png"
+            alt=""
+          />
+          <span>
+            <a class="text-xsmall" href="profile.html">Ray-Nathan James</a>
+            <p class="text-faded text-xsmall">1 month ago</p>
+          </span>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import data from '../data.json'
-import ThreadPage from '@/components/ThreadPage'
+import data from "../data.json";
+import ThreadPage from "@/components/ThreadPage";
 
 export default {
-  name: 'ThreadsPage',
+  name: "ThreadsPage",
   components: {
     ThreadPage
   },
-  data () {
+  data() {
     return {
       threads: data.threads,
       posts: data.posts,
       users: data.users
-    }
+    };
   }
-}
+};
 </script>
