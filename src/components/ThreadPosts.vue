08ca9513b16d2eb3be9bdb3dec8f48854b1a12e9
Alex Dorogensky <alex@exebar.com>
2020-09-29 08:43:35 -0500

    ThreadPosts.vue: display post' publishedAt timestamp filtered to show a date with new module 'moment'

--- src/components/ThreadPosts.vue
+++ src/components/ThreadPosts.vue
@@ -3,18 +3,22 @@
         <div v-for="post in posts" :key="post['.key']" class="post">
             <thread-post-user :user="users[post.userId]" />
             <thread-post :post="post" />
-            <div class="post-date text-faded">
-                6 hours ago
-            </div>
+            <div class="post-date text-faded">{{ post.publishedAt | asDate }}</div>
         </div>
     </div>
 </template>
 <script>
 import ThreadPostUser from '@/components/ThreadPostUser'
 import ThreadPost from '@/components/ThreadPost'
+import moment from 'moment'
 export default {
     name: 'ThreadPosts',
     props: ['users', 'posts'],
-    components: { ThreadPostUser, ThreadPost }
+    components: { ThreadPostUser, ThreadPost },
+    filters: {
+        asDate(timestamp) {
+            return moment.unix(timestamp).format('MMMM Do YYYY, h:mm:ss a');
+        }
+    }
 }
 </script>
