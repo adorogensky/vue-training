9b26e365a60b2e540c0ca6e0fa07946a7876d6eb
Alex Dorogensky <alex@exebar.com>
2020-09-29 10:07:10 -0500

    ThreadPosts.vue: use new component ThreadPostDate.vue

---
+++ src/components/ThreadPostDate.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="post-date text-faded">{{ timestamp | asDate }}</div>
+</template>
+<script>
+import moment from 'moment'
+export default {
+    props: ['timestamp'],
+    filters: {
+        asDate(timestamp) {
+            return moment.unix(timestamp).format('MMMM Do YYYY, h:mm:ss a');
+        }
+    }
+}
+</script>
--- src/components/ThreadPosts.vue
+++ src/components/ThreadPosts.vue
@@ -3,22 +3,17 @@
         <div v-for="post in posts" :key="post['.key']" class="post">
             <thread-post-user :user="users[post.userId]" />
             <thread-post :post="post" />
-            <div class="post-date text-faded">{{ post.publishedAt | asDate }}</div>
+            <thread-post-date :timestamp="post.publishedAt" />
         </div>
     </div>
 </template>
 <script>
 import ThreadPostUser from '@/components/ThreadPostUser'
 import ThreadPost from '@/components/ThreadPost'
-import moment from 'moment'
+import ThreadPostDate from '@/components/ThreadPostDate'
 export default {
     name: 'ThreadPosts',
     props: ['users', 'posts'],
-    components: { ThreadPostUser, ThreadPost },
-    filters: {
-        asDate(timestamp) {
-            return moment.unix(timestamp).format('MMMM Do YYYY, h:mm:ss a');
-        }
-    }
+    components: { ThreadPostUser, ThreadPost, ThreadPostDate }
 }
 </script>
